ef0e0fc696aae1cac4585ece23e3813351f689d5
metaleap <philipp.schumann@gmail.com>
2013-03-03 16:35:29 +0700

    ..

--- dom/util/fxsampler.go
+++ dom/util/fxsampler.go
@@ -2,12 +2,13 @@ package cdomutil
 
 import (
 	cdom "github.com/go3d/go-collada/dom"
+	ugfx "github.com/metaleap/go-util/gfx"
 )
 
 //	Creates and returns a new FxSampler of cdom.FxSamplerKind2D, sampling the specified
 //	image with the specified filtering (or cdom.DefaultFxSamplerFiltering if nil) and the
 //	specified wrapping (or cdom.DefaultFxSamplerWrapping if nil).
-func NewFxSampler2D(image *cdom.FxImageInst, filtering *cdom.FxSamplerFiltering, wrapping *cdom.FxSamplerWrapping) (me *cdom.FxSampler) {
+func NewFxSampler2D(image *cdom.FxImageInst, filtering *cdom.FxSamplerFiltering, wrapping *ugfx.SamplerWrapping) (me *cdom.FxSampler) {
 	me = cdom.NewFxSampler()
 	if filtering != nil {
 		me.Filtering = filtering
